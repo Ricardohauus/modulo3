50fe7842913232593964ea29e618b9de51bf69f8
Ricardo Brasil <ricardo@grupocm.net>
2019-03-27 18:52:34 -0300

    Atualizado até a aula 17

--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,18 @@
 const nodemailer = require('nodemailer')
 const mailConfig = require('../../config/mail')
+const hbs = require('nodemailer-express-handlebars')
 
 const transport = nodemailer.createTransport(mailConfig)
+const config = {
+  viewEngine: {
+    extName: '.hbs',
+    partialsDir: 'src/app/views/emails',
+    layoutsDir: 'src/app/views/emails',
+    defaultLayout: 'purchase.hbs'
+  },
+  viewPath: 'src/app/views/emails',
+  extName: '.hbs'
+}
+transport.use('compile', hbs(config))
 
 module.exports = transport
